3249e4b7318fcbaad821dcb1c5504de67ff6e565
James Maina <preprxnursing@gmail.com>
2026-04-26 06:44:21 -0800

    Clip logo frame, scale content only

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -517,7 +517,7 @@ export default function Navbar() {
           <Link href="/" style={{ flexShrink: 0, display: "flex", alignItems: "center", textDecoration: "none", opacity: .92, transition: "opacity .15s" }}
             onMouseEnter={e => (e.currentTarget.style.opacity = "1")}
             onMouseLeave={e => (e.currentTarget.style.opacity = ".92")}>
-            <Image src="/logo.png" alt="Pre-NCLEX Nursing" width={130} height={34} style={{ objectFit: "cover", objectPosition: "center", transform: "scale(1.3)", transformOrigin: "center" }} />
+            <div style={{ overflow: "hidden", width: "130px", height: "34px", flexShrink: 0 }}><Image src="/logo.png" alt="Pre-NCLEX Nursing" width={130} height={34} style={{ objectFit: "contain", transform: "scale(1.4)", transformOrigin: "center" }} /></div>
           </Link>
 
           {/* DESKTOP NAV */}
@@ -799,6 +799,9 @@ export default function Navbar() {
     </>
   );
 }
+
+
+
 
 
 
