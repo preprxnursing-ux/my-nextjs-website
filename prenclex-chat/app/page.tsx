4ea4b91070318cb664e17ec26253c8c765b45d6e
James Maina <preprxnursing@gmail.com>
2026-04-27 03:44:47 -0800

    fix: remove prenclex-chat from build

--- prenclex-chat/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import NursingChatbot from "@/components/NursingChatbot";
-
-export default function Home() {
-  return (
-    <main>
-      <NursingChatbot userId="guest" />
-    </main>
-  );
-}
--- prenclex-chat/write-chatbot.py
+++
@@ -1,4 +0,0 @@
-import os
-dest = r"C:\Users\USER\Desktop\nclex-app\prenclex-chat\components\NursingChatbot.jsx"
-print("File will be written to:", dest)
-print("Ready")
