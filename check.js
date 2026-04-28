2687eef886e896daea8183ada4083471702cd34f
James Maina <preprxnursing@gmail.com>
2026-04-27 21:06:35 -0800

    fix: navbar sticky + hidden on mobile

--- check.js
+++ check.js
@@ -1,5 +1,4 @@
 ﻿const fs = require('fs');
-const f = fs.readFileSync('components/MobileNav.tsx','utf8');
-const i = f.indexOf('mob-hero-visitor');
-const j = f.indexOf('mob-hero-visitor', i+1);
-console.log(f.slice(j-10, j+600));
+const f = fs.readFileSync('components/Navbar.tsx','utf8');
+const i = f.indexOf('<header');
+console.log(f.slice(i, i+120));
---
+++ fix3.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('components/Navbar.tsx','utf8');
+f = f.replace(
+  '<header style={{ position: "fixed", top: 0, left: 0, right: 0, width: "100%", zIndex: 1000,',
+  '<header className="mob-hide-on-mobile" style={{ position: "sticky", top: 0, zIndex: 1000,'
+);
+fs.writeFileSync('components/Navbar.tsx', f, 'utf8');
+console.log('Done');
