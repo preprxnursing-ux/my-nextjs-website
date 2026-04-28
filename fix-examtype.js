3843b9d0442a106dd17c0531d861aa6f468152bd
James Maina <preprxnursing@gmail.com>
2026-04-28 06:41:02 -0800

    fix: use course.exam instead of examType in Try Quiz links

---
+++ fix-examtype.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('components/Navbar.tsx','utf8');
+f = f.replace(/course\.examType/g, 'course.exam.replace("(R)","")');
+fs.writeFileSync('components/Navbar.tsx', f, 'utf8');
+console.log('Done');
