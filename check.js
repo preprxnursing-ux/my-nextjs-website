23682ccc050c3df79c16590ed1e55adcfab8cd96
James Maina <preprxnursing@gmail.com>
2026-04-28 06:50:55 -0800

    fix: replace all course.examType with course.exam

--- check.js
+++ check.js
@@ -1,4 +1,4 @@
 ﻿const fs = require('fs');
 const f = fs.readFileSync('components/Navbar.tsx','utf8');
-const i = f.indexOf('appLinks.map');
-console.log(JSON.stringify(f.slice(i-5, i+200)));
+const i = f.indexOf('course.exam');
+console.log(JSON.stringify(f.slice(i-20, i+100)));
---
+++ fix-et2.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('components/Navbar.tsx','utf8');
+// Replace all remaining examType references with course.exam
+f = f.split('course.examType').join('course.exam.replace("(R)","")');
+fs.writeFileSync('components/Navbar.tsx', f, 'utf8');
+console.log('Count fixed:', (f.match(/course\.exam\.replace/g)||[]).length);
