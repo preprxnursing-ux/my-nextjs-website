ebdcd5e1177ef82ec0399c6f5c7900a586a70096
James Maina <preprxnursing@gmail.com>
2026-04-28 06:57:40 -0800

    fix: Nursing TV before AI Tutor

---
+++ fix-tv2.js
@@ -0,0 +1,15 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('components/Navbar.tsx','utf8');
+
+f = f.replace(
+  '<ContactDropdown pathname={pathname} />\r\n                <NursingTVDropdown pathname={pathname} />',
+  '<ContactDropdown pathname={pathname} />'
+);
+
+f = f.replace(
+  ' {appLinks.map((link) => (',
+  ' <NursingTVDropdown pathname={pathname} />\r\n                {appLinks.map((link) => ('
+);
+
+fs.writeFileSync('components/Navbar.tsx', f, 'utf8');
+console.log('Done');
