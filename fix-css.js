fcabcad7e085c4171e09e93bdb1dfb2c79b8d2fe
James Maina <preprxnursing@gmail.com>
2026-04-27 21:27:49 -0800

    fix: hide desktop header on mobile via CSS only

---
+++ fix-css.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('app/globals.css','utf8');
+f = f.replace(
+  '@media (max-width: 767px) {\n  \n  .mob-hide-on-mobile { display: none !important; }',
+  '@media (max-width: 767px) {\n  \n  .mob-hide-on-mobile { display: none !important; }\n  body > header, #__next > header { display: none !important; }'
+);
+fs.writeFileSync('app/globals.css', f, 'utf8');
+console.log('Done');
