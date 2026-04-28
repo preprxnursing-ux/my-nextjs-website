bf8646975a6f8d7dd5aa0811b7633fd735285250
James Maina <preprxnursing@gmail.com>
2026-04-27 21:15:16 -0800

    fix: hide desktop navbar on mobile only

---
+++ fix-mob.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('components/Navbar.tsx','utf8');
+f = f.replace('<header style={{', '<header className="mob-hide-on-mobile" style={{');
+fs.writeFileSync('components/Navbar.tsx', f, 'utf8');
+console.log('Done');
