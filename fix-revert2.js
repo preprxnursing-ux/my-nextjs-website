8aec6dc4a513c260c79f1c56178e9ff3310282bb
James Maina <preprxnursing@gmail.com>
2026-04-27 20:29:38 -0800

    revert: navbar back to sticky, remove paddingTop, restore overflow-x hidden

---
+++ fix-revert2.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('app/layout.tsx','utf8');
+f = f.replace('overflowX: "hidden", paddingTop: "64px"', 'overflowX: "hidden"');
+fs.writeFileSync('app/layout.tsx', f, 'utf8');
+console.log('Done');
---
+++ fix-revert3.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('app/globals.css','utf8');
+f = f.replace('body { overflow-x: clip; max-width: 100vw; }', 'html, body { overflow-x: hidden; max-width: 100vw; }');
+f = f.replace('body { overflow-x: clip !important; }', 'body, html { overflow-x: hidden !important; }');
+fs.writeFileSync('app/globals.css', f, 'utf8');
+console.log('Done');
