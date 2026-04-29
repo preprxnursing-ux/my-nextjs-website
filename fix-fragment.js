0e467815ff8506d1c163ecc860affca49a61fa1f
James Maina <preprxnursing@gmail.com>
2026-04-29 12:33:03 -0800

    cleanup: remove junk files

--- fix-fragment.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let c = fs.readFileSync('components/Navbar.tsx', 'utf8');
-c = c.replace(
-  /(<ProfileDropdown pathname=\{pathname\} \/>[\s\S]*?<\/div>\s*\n\s*\}\s*\n\s*\)\s*:\s*\()/,
-  (m) => m.replace(') : (', '\n              </>) : (')
-);
-fs.writeFileSync('components/Navbar.tsx', c, 'utf8');
-console.log('Done');
