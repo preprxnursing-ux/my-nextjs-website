4a678f0e119aaf809f6f6f607cfe0b3687b313ee
James Maina <preprxnursing@gmail.com>
2026-04-29 12:24:45 -0800

    restore: navbar from backup

---
+++ fix-jsx.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('components/Navbar.tsx', 'utf8');
+// Fix the broken fragment closing
+c = c.replace(/(<\/Link>\s*\n\s*<\/>)\s*\n\s*\}\)/, '<\/Link>\n              <\/>\n            })\n          }');
+fs.writeFileSync('components/Navbar.tsx', c, 'utf8');
+console.log('Done');
